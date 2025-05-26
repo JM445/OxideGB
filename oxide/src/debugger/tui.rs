fd000232df058e5d8f1f6f65e1c59c89bcc4f19c
Jean-Malo Meichel <jean-malo.meichel@epita.fr>
2025-05-26 23:14:05 +0200

    WIP: TUI learning

--- oxide/src/debugger/tui.rs
+++ oxide/src/debugger/tui.rs
@@ -1,56 +1,36 @@
 use crate::emulator::*;
 use super::*;
 
+use std::path::Path;
 use ratatui::{
-    widgets::Block,
-}
+    layout::{Constraint, Direction, Layout, Rect},
+    widgets::{Block, Borders, Paragraph},
+    text::Line,
+    Frame,
+};
 
 pub struct Ui {
     exit: bool,
-    asm: Block,
-    regs: Block,
-    mem: Block,
-    cmd: Block,
+    emulator: Emulator,
 }
 
 impl Ui {
-    pub fn new() {
+    pub fn new(emu: Emulator) -> Ui {
         Ui {
             exit: false,
-            asm: Block{},
-            regs: Self::def_regs(),
-            mem: Block{},
+            emulator: emu
         }
     }
+}
 
-    fn def_regs() -> Block {
-        Block::default()
-            .title(Line::from("Registers").left_aligned())
-            .borders(Borders::ALL)
+impl Ui {
+    fn draw(&self, frame: &mut Frame) {
+        let area = frame.area();
+
     }
-
-    fn def_asm() -> Block {
-        Block::default()
-            .title(Line::from("Dissassembly").left_aligned())
-            .borders(Borders::ALL)
-    }
-
-    fn def_memory() -> Block {
-        Block::default()
-            .title(Line::from("Memory Content").left_aligned())
-            .borders(Borders::ALL)
-    }
-
-    fn def_cmd() -> Block {
-        Block::default()
-            .title(Line::from("Command Line").left_aligned())
-            .borders(Borders::ALL)
-    }
-
 }
 
 pub fn tui_main<P: AsRef<Path>>(rom_path: P) -> Result<(), String> {
-    let mut emulator = Emulator::new();
-    let mut ui = Ui::new();
-
+    let mut ui = Ui::new(Emulator::new(rom_path));
+    Ok(())
 }
