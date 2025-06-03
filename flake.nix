172f443ef16ec2f935a078a6d2d1f35fc9484b97
Jean-Malo Meichel <jean-malo.meichel@epita.fr>
2025-06-03 21:57:09 +0200

    Shell: Added hook to kill emacs if no client connected

--- flake.nix
+++ flake.nix
@@ -3,17 +3,21 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem(system:
-        let pkgs = import nixpkgs { inherit system; };
+        let pkgs = import nixpkgs {
+              inherit system;
+              overlays = [rust-overlay.overlays.default];
+            };
+            toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
         in
         {
           devShell = pkgs.mkShell {
-            buildInputs = [
-              pkgs.cargo
-              pkgs.rustc
-              pkgs.rustfmt
+            packages = [
+              toolchain
+              pkgs.rust-analyzer-unwrapped
 
               # Necessary for the openssl-sys crate:
               pkgs.openssl
@@ -26,6 +30,23 @@
               pkgs.python312
               pkgs.python312Packages.requests
             ];
+
+            RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+
+            shellHook = ''
+if emacsclient -e "(emacs-pid)" >/dev/null 2>&1; then
+    # Emacs daemon is running
+    if [ "$(emacsclient -e '(length (frame-list))' 2>/dev/null | tr -d '\"')" -le 1 ]; then
+      # No GUI or TTY client connected (only the initial frame)
+      echo "Killing Emacs daemon for updated environment..."
+      emacsclient -e '(kill-emacs)' >/dev/null 2>&1
+    else
+      echo "Emacs clients are connected; skipping daemon kill."
+    fi
+  else
+    echo "No emacs daemon running, nothing to kill"
+  fi
+'';
           };
         }
       );
